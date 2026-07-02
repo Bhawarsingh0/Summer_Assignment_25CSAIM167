53ac36c01905e4da7091cb9e2a821d9dd36ac375
Bhawarsingh0 <bholach378@gmail.com>
2026-07-02 20:04:32 +0530

    Day-23 completed

---
+++ Day-23/Q92.C
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int freq[256] = {0};
+    int i, max = 0;
+    char ch;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    // Count frequency of each character
+    for (i = 0; str[i] != '\0'; i++) {
+        freq[(unsigned char)str[i]]++;
+    }
+
+    // Find the maximum occurring character
+    for (i = 0; str[i] != '\0'; i++) {
+        if (freq[(unsigned char)str[i]] > max) {
+            max = freq[(unsigned char)str[i]];
+            ch = str[i];
+        }
+    }
+
+    printf("Maximum occurring character = %c\n", ch);
+    printf("Frequency = %d\n", max);
+
+    return 0;
+}

976c3d20f9cf451da0e4e88cfb0dd7966ccac468
Bhawarsingh0 <bholach378@gmail.com>
2026-07-08 21:22:33 +0530

    Day-29 completed

---
+++ Day-29/Q115.C
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100], temp[100];
+    int choice, i, len;
+    char ch;
+
+    printf("Enter first string: ");
+    scanf("%s", str1);
+
+    printf("Enter second string: ");
+    scanf("%s", str2);
+
+    do {
+        printf("\n===== MENU DRIVEN STRING OPERATIONS =====\n");
+        printf("1. Find Length of First String\n");
+        printf("2. Copy First String\n");
+        printf("3. Concatenate Strings\n");
+        printf("4. Compare Strings\n");
+        printf("5. Reverse First String\n");
+        printf("6. Display Strings\n");
+        printf("7. Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+
+            case 1:
+                printf("Length = %lu\n", strlen(str1));
+                break;
+
+            case 2:
+                strcpy(temp, str1);
+                printf("Copied String = %s\n", temp);
+                break;
+
+            case 3:
+                strcpy(temp, str1);
+                strcat(temp, str2);
+                printf("Concatenated String = %s\n", temp);
+                break;
+
+            case 4:
+                if(strcmp(str1, str2) == 0)
+                    printf("Strings are Equal.\n");
+                else
+                    printf("Strings are Not Equal.\n");
+                break;
+
+            case 5:
+                strcpy(temp, str1);
+                len = strlen(temp);
+
+                for(i = 0; i < len / 2; i++) {
+                    ch = temp[i];
+                    temp[i] = temp[len - i - 1];
+                    temp[len - i - 1] = ch;
+                }
+
+                printf("Reversed String = %s\n", temp);
+                break;
+
+            case 6:
+                printf("First String  : %s\n", str1);
+                printf("Second String : %s\n", str2);
+                break;
+
+            case 7:
+                printf("Exiting Program...\n");
+                break;
+
+            default:
+                printf("Invalid Choice!\n");
+        }
+
+    } while(choice != 7);
+
+    return 0;
+}
